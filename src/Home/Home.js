5253b590a0075ce04fd4c26ccc142a450cde5c74
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-09-28 22:40:07 +0600

    fontawesome added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     return (
         <div>
             <h1>I am from home</h1>
+            <FontAwesomeIcon icon={faCoffee} />
         </div>
     );
 };
