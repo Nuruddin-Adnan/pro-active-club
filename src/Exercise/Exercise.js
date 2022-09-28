9d981fe9dacdc6fe1027b0c4769819dc9b47588f
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-09-29 02:03:54 +0600

    exercise card data ui show

--- src/Exercise/Exercise.js
+++ src/Exercise/Exercise.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Exercise = ({ exercise }) => {
     const { id, img, activityName, description, ageLimit, time } = exercise;
     return (
-        <div className='bg-white rounded-lg p-5 border'>
+        <div className='bg-white rounded-lg p-5 pb-24 border relative'>
             <img src={img} alt="exercise pic" className='rounded' />
             <h3 className='text-2xl font-semibold py-4'>{activityName}</h3>
             <p className='text-base text-slate-500 font-normal'>{description}</p>
-            <p className='text-xl'>For Age: <strong>{ageLimit}</strong></p>
-            <p>Time required :  <span>{time}</span></p>
+            <p className='text-base pt-3 pb-1'>For Age: <span className='font-medium'>{ageLimit}</span></p>
+            <p className='text-base'>Time required :  <span className='font-medium'>{time}</span></p>
+            <button className='bg-red-500 hover:bg-red-600 text-white font-medium text-lg rounded-lg p-3 absolute bottom-5 left-2/4' style={{ transform: 'translateX(-50%)', width: 'calc(100% - 40px)' }}>Add to list</button>
         </div>
     );
 };
